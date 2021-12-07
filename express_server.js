7d388a889b50bb554a4830a11bc4d9b335071bbc
NickCoconut <gelaxyy@gmail.com>
2021-12-07 06:40:42 +0000

    redirectShortUrl

--- express_server.js
+++ express_server.js
@@ -8,35 +8,42 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 function generateRandomString() {
-  const urlId = Math.random().toString(36).substr(2, 6);
-  return urlId;
+  const shortURL = Math.random().toString(36).substr(2, 6);
+  return shortURL;
 }
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-} 
-
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new')
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
+};
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(`/urls/${shortURL}`);         
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(req.params)
   const shortURL = req.params.shortURL
   const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
-  res.render("urls_show", templateVars);
+  console.log(templateVars);
+  res.render('urls_show', templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
