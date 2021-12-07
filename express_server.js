781d94067a559d04ff4f1fda2ab4675f463faff8
NickCoconut <gelaxyy@gmail.com>
2021-12-07 02:54:16 +0000

    addForm

--- express_server.js
+++ express_server.js
@@ -4,15 +4,33 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  const urlId = Math.random().toString(36).substr(2, 6);
+  return urlId;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 } 
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render('urls_index', templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(req.params)
